fd61b60cc8b44c40c60293c7570405d68dc64ce5
Florian Hergenhahn <management@flouet.com>
2020-06-01 14:18:01 +0200

    StarAction main menu cleanup

--- StarAction/js/mainMenu.js
+++ StarAction/js/mainMenu.js
@@ -1,20 +1,23 @@
 /*Funktion för att starta spelet*/
 function startGame(){
-    var gameScene = "../Gamescene/index.html";
+    const gameScene = "../Gamescene/index.html";
     window.location = gameScene;
 }
+
 /*Funktion för att ta sig till information angående kontroler osv */
 function controlInfo(){
-    var controlScene ="../ControlScenes/control.html"
+    const controlScene ="../control.html"
     window.location =controlScene;
 }
+
 /*Funktion för att ta sig till The Collection, som innehåller alla programmeringsuppgifter osv */
 function goBackToMenu(){
-    var theCollection ="../../index.html"
+    const theCollection ="../../index.html"
     window.location = theCollection;
 }
+
 /*Funktion för att ta sig till kontaktformuläret */
 function contact(){
-    var contactForm ="https://www.timworksupport.tech/#contact"
+    const contactForm ="https://www.timworksupport.tech/#contact"
     window.location = contactForm;
 }
